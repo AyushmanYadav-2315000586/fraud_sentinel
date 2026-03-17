1722ee572b86dd44bad1faafa9d4276ba40a147c
Ayushman Yadav <ayushmanyadav80@gmail.com>
2026-03-17 16:09:53 +0530

    [Ayushman] Update . App.ts and verify database connection

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,25 +1,51 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
+import db from "./config/db.js";
+import { users } from "./schema/user.schema.js";
 
 // Create an instance of the Express application
-const app : any = express();
+const app: any = express();
 
 /**
  * All the middlewares will be defined here.
  */
 app.use(express.json());
-app.use(cors({
-    origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-}));
-
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 
 /**
  * Routes for API endpoints will be defined here.
  */
-app.get('/', (req : any,res : any)=>{
-    res.send('Welcome to Fraud Sentinel API..');
+app.get("/", (req: any, res: any) => {
+  res.send("Welcome to Fraud Sentinel API..");
+});
+
+app.get("/db-test", async (req:any, res:any) => {
+  try {
+    const result = await db.execute("SELECT 1");
+    res.json({ success: true, result });
+  } catch (error) {
+    res.json({ success: false, error });
+  }
+});
+
+app.get("/test-insert", async (req:any, res:any) => {
+  try {
+    await db.insert(users).values({
+        upiId: "8864809531@ptyes",
+        avgTransactionAmount: 500,
+        totalTransactions: 1,
+        knownDevices: ["IQOO NEO 6"]
+    });
+    res.send("User inserted successfully");
+  } catch (error) {
+    res.send(error);
+  }
 });
 
 // Export the app instance to be used in server.ts
